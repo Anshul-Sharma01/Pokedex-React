a323ec4835470bab242a0a39cd08d9e2b1fdeb2a
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-06-12 10:31:40 +0530

    added pagination functionality

--- src/components/PokeList/PokeList.jsx
+++ src/components/PokeList/PokeList.jsx
@@ -7,12 +7,19 @@ function PokeList(){
 
     const [pokemonList, setPokemonList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
-    const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
+    const [pokedexURL, setPokedexURL] = useState("https://pokeapi.co/api/v2/pokemon");
+    const [prevURL, setPrevURL ] = useState("");
+    const [nextURL, setNextURL ] = useState("");
 
     async function PokemonData(){
-        const response = await axios.get(POKEDEX_URL);
+        setIsLoading(true);
+        const response = await axios.get(pokedexURL);
         const pokeResult = response.data.results;
+        // console.log("data : ", response.data);
+        // console.log("Prev : ", response.data.previous);
+        // console.log("Next : ", response.data.next);
+        setPrevURL(response.data.previous);
+        setNextURL(response.data.next);
         const pokeResultPromise = pokeResult.map((pokemon) => axios.get(pokemon.url));
         const pokeData = await axios.all(pokeResultPromise);
         const res = (pokeData.map((pokemonData) => {
@@ -26,13 +33,18 @@ function PokeList(){
 
     useEffect(() => {
         PokemonData();
-    },[])
+    },[pokedexURL]);
 
     return(
         <div className="m-auto flex flex-col items-center">
             <h1 className="text-2xl font-bold mb-4">PokeList</h1>
-            <div className="flex flex-row justify-center items-center flex-wrap gap-20 md:grid-cols-2 lg:grid-cols-3 ">
+            <div className="flex flex-row justify-center  items-center flex-wrap gap-20 md:grid-cols-2 lg:grid-cols-3">
                 {isLoading ? <Loader/> : pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id}/>)}
+            </div>
+            <div className=" flex gap-10 p-10 m-5">
+                
+                <button className="bg-orange-300 px-6 py-3 text-black font-bold font-mono  border-solid border-black border-2 hover:bg-orange-700 hover:text-white" disabled={prevURL == undefined} onClick={() => setPokedexURL(prevURL) }  >Prev</button>
+                <button className="bg-orange-300 px-6 py-3 text-black font-bold font-mono  border-solid border-black border-2 hover:bg-orange-700 hover:text-white" disabled={nextURL == undefined}  onClick={() =>  setPokedexURL(nextURL) }>Next</button>
             </div>
         </div>
     )
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,7 +1,7 @@
 function Pokemon({name, image}){
     return(
-        <div className="flex flex-col justify-center items-center border p-4 rounded shadow-md">
-            <div className="font-bold mb-2">{name}</div>
+        <div className="flex flex-col justify-center items-center border p-4 rounded shadow-md hover:cursor-pointer hover:bg-slate-200">
+            <div className="font-bold mb-2 tracking-widest">{name}</div>
             <div><img src={image} alt={name} className="w-32 h-32  object-contain"/></div>
         </div>
     )
